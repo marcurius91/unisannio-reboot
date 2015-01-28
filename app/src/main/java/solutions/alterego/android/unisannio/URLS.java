b6c853385f07f5afbf76028f079520ed927b5db9
Ivan Morgillo <imorgillo@gmail.com>
2015-01-28 11:25:44 +0100

    Add URLS

---
+++ app/src/main/java/solutions/alterego/android/unisannio/URLS.java
@@ -0,0 +1,34 @@
+package solutions.alterego.android.unisannio;
+
+public class URLS {
+
+    public static final String ATENEO = "http://www.unisannio.it";
+
+    public static final String ATENEO_NEWS = "http://www.unisannio.it/notizie/avvisi/index.php";
+
+    public static final String ATENEO_DETAIL_BASE_URL = "http://www.unisannio.it/notizie/avvisi/viscom.php?id=";
+
+    public static final String SCIENZE_NEWS = "http://www.sciunisannio.it";
+
+    public static final String SCIENZE = SCIENZE_NEWS;
+
+    public static final String INGEGNERIA = "http://www.ing.unisannio.it";
+
+    public static final String INGEGNERIA_NEWS_RSS = "http://www.ing.unisannio.it/avvisi/rss20.xml";
+
+    public static final String INGEGNERIA_NEWS_PHP = "http://www.ing.unisannio.it/avvisi/index.php";
+
+    public static final String INGEGNERIA_EVENTI = "http://www.ing.unisannio.it/eventi/index.html";
+
+    public static final String INGEGNERIA_EVENTI_BASE_URL = "http://www.ing.unisannio.it/eventi/";
+
+    public static final String GIURISPRUDENZA = "http://www.giurisprudenza.unisannio.it";
+
+    public static final String GIURISPRUDENZA_AVVISI = "http://www.giurisprudenza.unisannio.it/index.php?option=com_rss&catid=2";
+
+    public static final String GIURISPRUDENZA_COMUNICAZIONI = "http://www.giurisprudenza.unisannio.it/index.php?option=com_rss&catid=1";
+
+    public static final String SEA_NEWS = "http://www.didatticademm.it/index.php?option=com_content&view=section&id=5&Itemid=65";
+
+    public static final String SEA = "http://www.didatticademm.it/";
+}
