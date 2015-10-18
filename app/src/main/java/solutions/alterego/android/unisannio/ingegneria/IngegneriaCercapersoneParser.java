d79c65b23358e858f370c0af80cfdc4b69b0f1fb
ShAdOwNeT88 <iovine.antonio.1988@gmail.com>
2015-10-18 11:47:16 +0200

    implemented IngegneriaCercapersoneParser

--- app/src/main/java/solutions/alterego/android/unisannio/ingegneria/IngegneriaCercapersoneParser.java
+++ app/src/main/java/solutions/alterego/android/unisannio/ingegneria/IngegneriaCercapersoneParser.java
@@ -1,21 +1,50 @@
 package solutions.alterego.android.unisannio.ingegneria;
 
+import android.util.Log;
+
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.util.List;
 
 import javax.inject.Inject;
 
+import solutions.alterego.android.unisannio.cercapersone.Person;
 import solutions.alterego.android.unisannio.interfaces.IParser;
 
-public class IngegneriaCercapersoneParser implements IParser<Document> {
+public class IngegneriaCercapersoneParser implements IParser<Person> {
 
     @Inject
     public IngegneriaCercapersoneParser() {
     }
 
     @Override
-    public List<Document> parse(Document document) {
-        return null;
+    public List<Person> parse(Document document) {
+        List<Person> persons = null;
+        Elements elements = document.getElementsByTag("item");
+
+        for (Element element:elements){
+
+                String nome = element.getElementsByTag("title").text();
+                String ruolo = element.getElementsByTag("position").text();
+                String email = element.getElementsByTag("email").text();
+                String ufficio = element.getElementsByTag("address").text();
+                String telefono = element.getElementsByTag("telephone").text();
+                String webPage = element.getElementsByTag("personalwebpage").text();
+                String tutoring = element.getElementsByTag("tutoring").text();
+
+                Person person = new Person(nome, ruolo, email, telefono, ufficio, webPage, tutoring);
+                persons.add(person);
+        }
+
+        if(persons != null){
+            return persons;
+        }
+        else{
+            Log.e("PERSON LIST","Empty persons");
+            return null;
+        }
+
     }
 }
