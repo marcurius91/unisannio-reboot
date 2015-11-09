cf61ed1fdb3fb2be203612f8d972a22eddbb99ac
ShAdOwNeT88 <iovine.antonio.1988@gmail.com>
2015-11-09 14:35:25 +0100

    Fixed poisition element at Detail_Cercapersone

--- app/src/main/java/solutions/alterego/android/unisannio/ingegneria/IngengeriaCercapersoneActivity.java
+++ app/src/main/java/solutions/alterego/android/unisannio/ingegneria/IngengeriaCercapersoneActivity.java
@@ -1,5 +1,6 @@
 package solutions.alterego.android.unisannio.ingegneria;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -30,10 +31,12 @@ import butterknife.Bind;
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
 import solutions.alterego.android.unisannio.MapsActivity;
+import solutions.alterego.android.unisannio.NavigationViewManager.NavigationViewManager;
 import solutions.alterego.android.unisannio.R;
 import solutions.alterego.android.unisannio.URLS;
 import solutions.alterego.android.unisannio.analytics.AnalyticsManager;
 import solutions.alterego.android.unisannio.analytics.Screen;
+import solutions.alterego.android.unisannio.ateneo.AteneoAvvisiFragment;
 import solutions.alterego.android.unisannio.cercapersone.CercapersoneAdapter;
 import solutions.alterego.android.unisannio.cercapersone.Person;
 import solutions.alterego.android.unisannio.cercapersone.SearchPerson;
@@ -56,10 +59,10 @@ public class IngengeriaCercapersoneActivity extends AppCompatActivity{
 
     private CercapersoneAdapter mAdapter;
     private ArrayList<Person> mPersons = new ArrayList<>();
-    private Intent mMap;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     private String personToSearch;
+    NavigationViewManager navigationViewManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,8 +76,6 @@ public class IngengeriaCercapersoneActivity extends AppCompatActivity{
 
         //Initializing NavigationView
         navigationView = (NavigationView) findViewById(R.id.navigation_view);
-
-        mMap = new Intent(this, MapsActivity.class);
 
         EditText tv_cercapersone = (EditText) findViewById(R.id.textView_ingegneria_cercapersone);
         Button btn_cercapersone = (Button) findViewById(R.id.button_ingegneria_cercapersone);
@@ -121,7 +122,7 @@ public class IngengeriaCercapersoneActivity extends AppCompatActivity{
 
                             @Override
                             public void onNext(ArrayList<Person> persons) {
-                                ArrayList<Person> prsSrc = SearchPerson.searchPerson(personToSearch,persons);
+                                ArrayList<Person> prsSrc = SearchPerson.searchPerson(personToSearch, persons);
                                 mAdapter.addPersons(prsSrc);
                                 mSwipeRefreshLayout.setRefreshing(false);
                             }
@@ -131,6 +132,7 @@ public class IngengeriaCercapersoneActivity extends AppCompatActivity{
 
         // Initializing Drawer Layout and ActionBarToggle
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout_cercapersone);
+
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, mToolbar, R.string.drawer_open,
                 R.string.drawer_close) {
 
@@ -150,130 +152,14 @@ public class IngengeriaCercapersoneActivity extends AppCompatActivity{
 
         //Setting the actionbarToggle to drawer layout
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
         //calling sync state is necessay or else your hamburger icon wont show up
         actionBarDrawerToggle.syncState();
 
-    }
+        //TODO Fix bug on next lines (We don't have a fragment to replace)
+        //getSupportFragmentManager().beginTransaction().replace(R.id.container, AteneoAvvisiFragment.newInstance(false)).commit();
+        //navigationViewManager = new NavigationViewManager(drawerLayout,this);
+        //navigationView = navigationViewManager.setUpNavigationDrawer(navigationView);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        String fragmentName = getVisibleFragmentName(getVisibleFragment());
-        Intent browserIntent;
-
-        //Ateneo
-        if(fragmentName.equalsIgnoreCase("AteneoAvvisiFragment")){
-            switch (id){
-                case R.id.action_web_page:
-                    //Log.e("Active Fragment",getVisibleFragmentName(getVisibleFragment()));
-                    mAnalyticsManager.track(new Screen(getString(R.string.ateneo), getString(R.string.sito_web)));
-                    browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(URLS.ATENEO));
-                    startActivity(browserIntent);
-                    break;
-                case R.id.action_map:
-                    //Log.e("Active Fragment",getVisibleFragmentName(getVisibleFragment()));
-                    mAnalyticsManager.track(new Screen(getString(R.string.ateneo), getString(R.string.mappa)));
-                    mMap.putParcelableArrayListExtra("MARKERS", ((ArrayList) UnisannioGeoData.ATENEO()));
-                    startActivity(mMap);
-                    break;
-            }
-        }
-
-        //Ingegneria
-        if(fragmentName.equalsIgnoreCase("IngegneriaAvvisiFragment")){
-            switch (id){
-                case R.id.action_web_page:
-                    //Log.e("Active Fragment",getVisibleFragmentName(getVisibleFragment()));
-                    mAnalyticsManager.track(new Screen(getString(R.string.ingegneria), getString(R.string.sito_web)));
-                    browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(URLS.INGEGNERIA));
-                    startActivity(browserIntent);
-                    break;
-                case R.id.action_map:
-                    //Log.e("Active Fragment",getVisibleFragmentName(getVisibleFragment()));
-                    mAnalyticsManager.track(new Screen(getString(R.string.ingegneria), getString(R.string.mappa)));
-                    mMap.putParcelableArrayListExtra("MARKERS", ((ArrayList) UnisannioGeoData.INGEGNERIA()));
-                    startActivity(mMap);
-                    break;
-            }
-        }
-
-        //Scienze e Tecnologie
-        if(fragmentName.equalsIgnoreCase("ScienzeAvvisiFragment")){
-            switch (id){
-                case R.id.action_web_page:
-                    //Log.e("Active Fragment",getVisibleFragmentName(getVisibleFragment()));
-                    mAnalyticsManager.track(new Screen(getString(R.string.scienze), getString(R.string.sito_web)));
-                    browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(URLS.SCIENZE));
-                    startActivity(browserIntent);
-                    break;
-                case R.id.action_map:
-                    //Log.e("Active Fragment",getVisibleFragmentName(getVisibleFragment()));
-                    mAnalyticsManager.track(new Screen(getString(R.string.scienze), getString(R.string.mappa)));
-                    mMap.putParcelableArrayListExtra("MARKERS", ((ArrayList) UnisannioGeoData.SCIENZE()));
-                    startActivity(mMap);
-                    break;
-            }
-        }
-
-        //Giurisprudenza
-        if(fragmentName.equalsIgnoreCase("GiurisprudenzaAvvisiFragment")){
-            switch (id){
-                case R.id.action_web_page:
-                    //Log.e("Active Fragment",getVisibleFragmentName(getVisibleFragment()));
-                    mAnalyticsManager.track(new Screen(getString(R.string.giurisprudenza), getString(R.string.sito_web)));
-                    browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(URLS.GIURISPRUDENZA));
-                    startActivity(browserIntent);
-                    break;
-                case R.id.action_map:
-                    //Log.e("Active Fragment",getVisibleFragmentName(getVisibleFragment()));
-                    mAnalyticsManager.track(new Screen(getString(R.string.giurisprudenza), getString(R.string.mappa)));
-                    mMap.putParcelableArrayListExtra("MARKERS", ((ArrayList) UnisannioGeoData.GIURISPRUDENZA()));
-                    startActivity(mMap);
-                    break;
-            }
-        }
-
-        //SEA
-        if(fragmentName.equalsIgnoreCase("SeaAvvisiFragment")){
-            switch (id){
-                case R.id.action_web_page:
-                    //Log.e("Active Fragment",getVisibleFragmentName(getVisibleFragment()));
-                    mAnalyticsManager.track(new Screen(getString(R.string.sea), getString(R.string.sito_web)));
-                    browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(URLS.SEA));
-                    startActivity(browserIntent);
-                    break;
-                case R.id.action_map:
-                    //Log.e("Active Fragment",getVisibleFragmentName(getVisibleFragment()));
-                    mAnalyticsManager.track(new Screen(getString(R.string.sea), getString(R.string.mappa)));
-                    mMap.putParcelableArrayListExtra("MARKERS", ((ArrayList) UnisannioGeoData.SEA()));
-                    startActivity(mMap);
-                    break;
-            }
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    //get the active Fragment
-    public Fragment getVisibleFragment(){
-        FragmentManager fragmentManager = IngengeriaCercapersoneActivity.this.getSupportFragmentManager();
-        List<Fragment> fragments = fragmentManager.getFragments();
-        for(Fragment fragment : fragments){
-            if(fragment != null && fragment.getUserVisibleHint())
-                return (Fragment)fragment;
-        }
-        return null;
-    }
-
-    //get the name of the active Fragment
-    public String getVisibleFragmentName(Fragment fragment){
-        String str,fragmentName;
-        str = fragment.toString();
-        StringTokenizer st = new StringTokenizer(str,"{");
-        fragmentName = st.nextToken();
-        return fragmentName;
     }
 }
