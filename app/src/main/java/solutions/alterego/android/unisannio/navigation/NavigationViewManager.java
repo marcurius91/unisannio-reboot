082a35cd1736fbb8df2763256c8752616a220da4
ShAdOwNeT88 <iovine.antonio.1988@gmail.com>
2015-12-12 13:56:04 +0100

    Add custom tab opening on AlterEgoSolution and Github in navigationDrawer

--- app/src/main/java/solutions/alterego/android/unisannio/navigation/NavigationViewManager.java
+++ app/src/main/java/solutions/alterego/android/unisannio/navigation/NavigationViewManager.java
@@ -1,9 +1,12 @@
 package solutions.alterego.android.unisannio.navigation;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -11,8 +14,16 @@ import android.support.v4.widget.DrawerLayout;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.chromium.customtabsclient.CustomTabsActivityHelper;
+
 import javax.inject.Inject;
 
+import butterknife.BindColor;
+import me.zhanghai.android.customtabshelper.CustomTabsHelperFragment;
 import solutions.alterego.android.unisannio.MainActivity;
 import solutions.alterego.android.unisannio.R;
 import solutions.alterego.android.unisannio.URLS;
@@ -25,7 +36,7 @@ import solutions.alterego.android.unisannio.ingegneria.IngengeriaCercapersoneAct
 import solutions.alterego.android.unisannio.scienze.ScienzeAvvisiFragment;
 import solutions.alterego.android.unisannio.sea.SeaAvvisiFragment;
 
-public class NavigationViewManager extends FragmentActivity{
+public class NavigationViewManager extends FragmentActivity {
 
     private DrawerLayout drawerLayout;
     public Context context;
@@ -35,13 +46,34 @@ public class NavigationViewManager extends FragmentActivity{
     @Inject
     AnalyticsManager mAnalyticsManager;
 
-    public NavigationViewManager(DrawerLayout drawerLayout,Context context){
+    @BindColor(R.color.primaryColor)
+    int mColorPrimary;
+
+    private CustomTabsHelperFragment mCustomTabsHelperFragment;
+
+    private CustomTabsIntent mCustomTabsIntent;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    public NavigationViewManager(DrawerLayout drawerLayout, Context context) {
         this.drawerLayout = drawerLayout;
         this.context = context;
 
 
     }
-    public NavigationView setUpNavigationDrawer(NavigationView navigationView){
+
+    public NavigationView setUpNavigationDrawer(NavigationView navigationView) {
+
+        mCustomTabsHelperFragment = CustomTabsHelperFragment.attachTo((FragmentActivity) context);
+        mCustomTabsIntent = new CustomTabsIntent.Builder()
+                .enableUrlBarHiding()
+                .setToolbarColor(mColorPrimary)
+                .setShowTitle(true)
+                .build();
+
 
         //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -96,7 +128,7 @@ public class NavigationViewManager extends FragmentActivity{
                         context.startActivity(cercapersoneIngegneria);
                         return true;
 
-                        // Scienze e tecnologie
+                    // Scienze e tecnologie
                     case R.id.avvisi_studenti_scienze_tecnologie:
                         //mAnalyticsManager.track(new Screen(context.getString(R.string.scienze), context.getString(R.string.avvisi_studenti)));
                         fragmentManager.beginTransaction()
@@ -129,13 +161,11 @@ public class NavigationViewManager extends FragmentActivity{
                     //About
                     case R.id.alteregosolution:
                         //mAnalyticsManager.track(new Screen(context.getString(R.string.about), context.getString(R.string.sito_web)));
-                        browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(URLS.ALTEREGO));
-                        context.startActivity(browserIntent);
+                        CustomTabsHelperFragment.open(((FragmentActivity) context),mCustomTabsIntent, Uri.parse(URLS.ALTEREGO), mCustomTabsFallback);
                         return true;
                     case R.id.github:
                         //mAnalyticsManager.track(new Screen(context.getString(R.string.about), context.getString(R.string.github)));
-                        browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(URLS.GITHUB));
-                        context.startActivity(browserIntent);
+                        CustomTabsHelperFragment.open(((FragmentActivity) context),mCustomTabsIntent, Uri.parse(URLS.GITHUB), mCustomTabsFallback);
                         return true;
 
                     default:
@@ -147,4 +177,67 @@ public class NavigationViewManager extends FragmentActivity{
         return navigationView;
     }
 
+    private final CustomTabsActivityHelper.CustomTabsFallback mCustomTabsFallback =
+            new CustomTabsActivityHelper.CustomTabsFallback() {
+                @Override
+                public void openUri(Activity activity, Uri uri) {
+                    Toast.makeText(activity, R.string.custom_tab_error, Toast.LENGTH_SHORT).show();
+                    try {
+                        activity.startActivity(new Intent(Intent.ACTION_VIEW, uri));
+                    } catch (ActivityNotFoundException e) {
+                        e.printStackTrace();
+                        Toast.makeText(activity, R.string.custom_tab_error_activity, Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                }
+            };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "NavigationViewManager Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://solutions.alterego.android.unisannio.navigation/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "NavigationViewManager Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://solutions.alterego.android.unisannio.navigation/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
