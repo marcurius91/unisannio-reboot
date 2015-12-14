838b472a3275515814f9135e15c2c34357e16aac
Ivan Morgillo <imorgillo@gmail.com>
2015-12-14 19:24:15 +0100

    Add first Unit Test PoC

---
+++ app/src/test/kotlin/solutions/alterego/android/TestUtils.kt
@@ -0,0 +1,13 @@
+package solutions.alterego.android
+
+import org.fest.assertions.api.Assertions
+import org.fest.assertions.api.ObjectAssert
+import org.mockito.Mockito
+
+fun <T> verify(mock: T): T {
+    return Mockito.verify(mock)
+}
+
+fun <T> assertThat(actual: T): ObjectAssert<T> {
+    return Assertions.assertThat(actual)
+}
---
+++ app/src/test/kotlin/solutions/alterego/android/unisannio/ingegneria/IngegneriaAvvisiStudentiParserTest.kt
@@ -0,0 +1,27 @@
+package solutions.alterego.android.unisannio.ingegneria
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.runners.MockitoJUnitRunner
+import kotlin.test.assertTrue
+
+@RunWith(MockitoJUnitRunner::class)
+class IngegneriaAvvisiStudentiParserTest {
+
+    @Before
+    fun setUp() {
+
+    }
+
+    @After
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun testParse() {
+        assertTrue(true)
+    }
+}
