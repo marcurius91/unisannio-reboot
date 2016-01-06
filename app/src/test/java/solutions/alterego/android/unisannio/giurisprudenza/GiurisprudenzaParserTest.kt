a67c1aa3ee4dd45c20891aae05550cdd54608bb5
ShAdOwNeT88 <iovine.antonio.1988@gmail.com>
2016-01-06 19:18:54 +0100

    add test for Article consistency

--- app/src/test/java/solutions/alterego/android/unisannio/giurisprudenza/GiurisprudenzaParserTest.kt
+++ app/src/test/java/solutions/alterego/android/unisannio/giurisprudenza/GiurisprudenzaParserTest.kt
@@ -5,6 +5,9 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import solutions.alterego.android.assertThat
+import solutions.alterego.android.unisannio.models.Article
+import java.text.SimpleDateFormat
+import kotlin.test.assertEquals
 
 class GiurisprudenzaParserTest {
 
@@ -28,5 +31,23 @@ class GiurisprudenzaParserTest {
     fun testNotNullParseDetail() {
         var list = parser.parse(document);
         assertThat(list).isNotNull;
+    }
+
+    @Test
+    fun testArticleListConsistency() {
+        var list = parser.parse(document);
+        var date = list.get(1).date;
+        var author = list.get(1).author;
+        var title = list.get(1).title;
+
+        //check if author and title are not null
+        assertThat(author).isNotNull;
+        assertThat(title).isNotNull;
+
+        //get year from date
+        var year = date.toString().substring(0,4).toInt();
+
+        //check if year is 2016
+        assertEquals(year,2016);
     }
 }
