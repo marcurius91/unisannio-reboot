7979c2cc4005225f2d5cbcfeaa2879922ebd389c
ShAdOwNeT88 <iovine.antonio.1988@gmail.com>
2017-06-26 14:02:32 +0200

    Fix Dipartimento Parser and added a method that extract month from date and convert into numeric.

--- app/src/main/java/solutions/alterego/android/unisannio/utils/DateUtils.java
+++ app/src/main/java/solutions/alterego/android/unisannio/utils/DateUtils.java
@@ -1,6 +1,9 @@
 package solutions.alterego.android.unisannio.utils;
 
+import android.util.Log;
+
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -38,6 +41,64 @@ public class DateUtils {
 
         return numbers;
 
+    }
+
+    public static String convertMonth(String date){
+
+        String date_temp = date.replace("Pubblicato:","");
+        date_temp = date_temp.replace(" ","/");
+
+        StringTokenizer st = new StringTokenizer(date_temp,"/");
+        String day = st.nextToken();
+        String month = st.nextToken();
+        String year = st.nextToken();
+
+        String final_date = null;
+
+        switch (month){
+            case "Gennaio":
+                month = "01";
+                break;
+            case "Febbraio":
+                month = "02";
+                break;
+            case "Marzo":
+                month = "03";
+                break;
+            case "Aprile":
+                month = "04";
+                break;
+            case "Maggio":
+                month = "05";
+                break;
+            case "Giugno":
+                month = "06";
+                break;
+            case "Luglio":
+                month = "07";
+                break;
+            case "Agosto":
+                month = "08";
+                break;
+            case "Settembre":
+                month = "09";
+                break;
+            case "Ottobre":
+                month = "10";
+                break;
+            case "Novembre":
+                month = "11";
+                break;
+            case "Dicembre":
+                month = "12";
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid month: " + month);
+        }
+
+        final_date = day.concat("/").concat(month).concat("/").concat(year);
+
+        return final_date;
     }
 
 
