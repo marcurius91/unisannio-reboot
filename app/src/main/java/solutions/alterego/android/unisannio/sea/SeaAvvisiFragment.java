cf92e0263d277ccd24d56c881a0aa5dc2716242d
enricofab <errico-fabry@libero.it>
2018-10-04 15:35:52 +0200

    SEa corretto(da testare)

--- app/src/main/java/solutions/alterego/android/unisannio/sea/SeaAvvisiFragment.java
+++ app/src/main/java/solutions/alterego/android/unisannio/sea/SeaAvvisiFragment.java
@@ -11,6 +11,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,12 +25,16 @@ import java.util.ArrayList;
 import javax.inject.Inject;
 
 import me.zhanghai.android.customtabshelper.CustomTabsHelperFragment;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
 import solutions.alterego.android.unisannio.App;
+import solutions.alterego.android.unisannio.DetailActivity;
 import solutions.alterego.android.unisannio.R;
 import solutions.alterego.android.unisannio.URLS;
 import solutions.alterego.android.unisannio.interfaces.OpenArticleDetailListener;
 import solutions.alterego.android.unisannio.models.Article;
 import solutions.alterego.android.unisannio.models.ArticleAdapter;
+import solutions.alterego.android.unisannio.scienze.ScienzeDetailPresenter;
 
 public class SeaAvvisiFragment extends Fragment {
 
@@ -81,9 +86,13 @@ public class SeaAvvisiFragment extends Fragment {
 
         mRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
 
+
+        final String url = URLS.SEA_NEWS;
+
+
         mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override public void onRefresh() {
-                SeaAvvisiFragment.this.refreshList();
+                SeaAvvisiFragment.this.refreshList(url);
             }
         });
 
@@ -95,9 +104,38 @@ public class SeaAvvisiFragment extends Fragment {
                 .build();
 
         mAdapter = new ArticleAdapter(new ArrayList<Article>(), new OpenArticleDetailListener() {
-            @Override public void openArticleDetail(@NonNull Article article, @NonNull RecyclerView.ViewHolder holder) {
-                String url1 = URLS.SEA + article.getUrl();
-                CustomTabsHelperFragment.open(SeaAvvisiFragment.this.getActivity(), mCustomTabsIntent, Uri.parse(url1), mCustomTabsFallback);
+            @Override public void openArticleDetail(@NonNull final Article article, @NonNull RecyclerView.ViewHolder holder) {
+
+                String url1 = URLS.SEA_NEWS + article.getUrl();
+                //CustomTabsHelperFragment.open(getActivity(), mCustomTabsIntent, Uri.parse(url1), mCustomTabsFallback);
+                SeaPresenter mDetailPresenter = new SeaPresenter(url1);
+
+                mDetailPresenter.getArticles().observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<ArrayList<Article>>() {
+                    @Override public void onCompleted() {
+
+                    }
+
+                    @Override public void onError(Throwable e) {
+                        Log.e("getBodyNews", e.toString());
+                    }
+
+                    @Override public void onNext(ArrayList<Article> bodys) {
+                        //article.setBody(bodys.get(0));
+                        Intent intent = new Intent();
+                        intent.setClass(getActivity(), DetailActivity.class);
+                        intent.putExtra("ARTICLE", article);
+                                       /*ActivityOptionsCompat options =
+                                               ActivityOptionsCompat.makeSceneTransitionAnimation(getActivity(),
+                                                       Pair.create(((ArticleAdapter.ViewHolder) holder).title, getString(R.string.transition_article_title)),
+                                                       Pair.create(((ArticleAdapter.ViewHolder) holder).date, getString(R.string.transition_article_date)),
+                                                       Pair.create(((ArticleAdapter.ViewHolder) holder).author, getString(R.string.transition_article_author))
+                                               );
+                                       ActivityCompat.startActivity(getActivity(), intent, options.toBundle());*/
+                        startActivity(intent);
+                    }
+                });
+               // String url1 = URLS.SEA + article.getUrl();
+                //CustomTabsHelperFragment.open(SeaAvvisiFragment.this.getActivity(), mCustomTabsIntent, Uri.parse(url1), mCustomTabsFallback);
             }
         },R.drawable.sea);
         mRecyclerView.setAdapter(mAdapter);
@@ -105,10 +143,10 @@ public class SeaAvvisiFragment extends Fragment {
         LinearLayoutManager layoutManager = new LinearLayoutManager(view.getContext());
         mRecyclerView.setLayoutManager(layoutManager);
 
-        refreshList();
+        refreshList(url);
     }
 
-    private void refreshList() {
+    private void refreshList(String url) {
         mRecyclerView.setVisibility(View.GONE);
         mSwipeRefreshLayout.setRefreshing(true);
 
--- app/src/main/java/solutions/alterego/android/unisannio/sea/SeaParser.java
+++ app/src/main/java/solutions/alterego/android/unisannio/sea/SeaParser.java
@@ -1,10 +1,14 @@
 package solutions.alterego.android.unisannio.sea;
 
+import android.net.Uri;
 import android.support.annotation.NonNull;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import org.joda.time.DateTime;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -21,12 +25,22 @@ public class SeaParser implements Parser<Article> {
         Elements elements = document.select("div.moduletable");
         Elements titles = elements.select("h4.jazin-title");
 
+       // Elements bodies=document.body().select("div#ja-current-content");//DA QUELLO INIZIALE DI SEA NEWS
+
         for (int i = 0; i < titles.size(); i++) {
-            Element link = titles.select("a").first();
+            Element link = titles.select("a").get(i);
             String url = URLS.SEA.concat(link.attr("href"));
             String title = titles.get(i).text();
-            Timber.d("LINK ARTICLE", url);
-            articles.add(new Article(UUID.randomUUID().toString(), title, "", link.attr("href"), "", ExtensionKt.toIso8601(DateTime.now())));
+
+
+
+
+
+           Element bodies=document.select("div#ja-current-content").first();
+            String body=bodies.text();
+
+
+            articles.add(new Article(UUID.randomUUID().toString(), title, "", url, document.toString(), ExtensionKt.toIso8601(DateTime.now())));
         }
 
         return articles;
