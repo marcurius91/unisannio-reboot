fb5297abd8bead5d8402e60afbf63fd8571cccda
francesco <procaccifrancesco93@gmail.com>
2018-10-18 17:49:31 +0200

    Creazione Test di Ateneo Retriever

---
+++ app/src/test/java/solutions/alterego/android/unisannio/ateneo/AteneoRetrieverTest.java
@@ -0,0 +1,29 @@
+package solutions.alterego.android.unisannio.ateneo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 667198 on 18/10/2018.
+ */
+public class AteneoRetrieverTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void retrieveDocument() throws Exception {
+    }
+
+    @Test
+    public void getDocument() throws Exception {
+    }
+
+}
